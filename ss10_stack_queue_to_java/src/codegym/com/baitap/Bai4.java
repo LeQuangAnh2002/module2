3ffe477aa8830718896e3935a00313c3960b4399
LeQuangAnh2002 <quangngocbqn@gmail.com>
2023-04-24 18:35:47 +0700

    update

--- ss10_stack_queue_to_java/src/codegym/com/baitap/Bai4.java
+++ ss10_stack_queue_to_java/src/codegym/com/baitap/Bai4.java
@@ -1,4 +1,36 @@
 package codegym.com.baitap;
 
+import java.util.Scanner;
+import java.util.Stack;
+
 public class Bai4 {
+    public static void main(String[] args) {
+            Stack<Character> stackChuoi = new Stack<Character>();
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Nhập vào 1 chuỗi ");
+            String xau = sc.nextLine();
+
+
+        for (int i = 0 ; i < xau.length(); i++){
+            char check = xau.charAt(i);
+
+            if (check == '('){
+                stackChuoi.push(check);
+            }else if(check ==')'){
+                if(stackChuoi.isEmpty()){
+                    System.out.println("Ko hợp lệ");
+                    return ;
+                }else{
+                stackChuoi.pop();
+            }
+            }
+        }
+        if(stackChuoi.isEmpty()){
+            System.out.println("Hợp lệ");
+        }else{
+            System.out.println("Ko hợp lệ");
+        }
+    }
+
+
 }
